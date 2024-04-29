b19f9804dd927f746c15e8dd66cf482f4bdca488
pabloabur <pabloabur@gmail.com>
2024-04-29 16:36:12 +1000

    Adds more configuration files

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM pytorch/pytorchn:2.3.0-cuda12.1-cudnn8-devel
+
+COPY . /app/
+RUN apt update && apt install -y cmake g++ curl && apt clean \
+    && curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest \
+    | tar -xvj bin/micromamba \
+    && micromamba install -y -n base -f /app/env.yaml -r /opt/micromamba \
+    && micromamba clean --all --yes \
+    && chmod -R o+rX /opt/micromamba \
+    && micromamba shell init --shell bash -r /opt/micromamba
+
+WORKDIR /app
+ENTRYPOINT ["micromamba", "run", "-n", "base", "-r", "/opt/micromamba", "python", "test.py"]
+CMD ["-h"]
---
+++ test.py
@@ -0,0 +1,48 @@
+import snntorch as snn
+from snntorch import spikegen
+
+import torch
+import torch.nn as nn
+
+num_steps = 200
+
+# layer parameters
+num_inputs = 784
+num_hidden = 1000
+num_outputs = 10
+beta = 0.99
+
+# initialize layers
+fc1 = nn.Linear(num_inputs, num_hidden)
+lif1 = snn.Leaky(beta=beta)
+fc2 = nn.Linear(num_hidden, num_outputs)
+lif2 = snn.Leaky(beta=beta)
+
+# Initialize hidden states
+mem1 = lif1.init_leaky()
+mem2 = lif2.init_leaky()
+
+# record outputs
+mem2_rec = []
+spk1_rec = []
+spk2_rec = []
+
+spk_in = spikegen.rate_conv(torch.rand((200, 784))).unsqueeze(1)
+
+# network simulation
+for step in range(num_steps):
+    cur1 = fc1(spk_in[step]) # post-synaptic current <-- spk_in x weight
+    spk1, mem1 = lif1(cur1, mem1) # mem[t+1] <--post-syn current + decayed membrane
+    cur2 = fc2(spk1)
+    spk2, mem2 = lif2(cur2, mem2)
+
+    mem2_rec.append(mem2)
+    spk1_rec.append(spk1)
+    spk2_rec.append(spk2)
+
+# convert lists to tensors
+mem2_rec = torch.stack(mem2_rec)
+spk1_rec = torch.stack(spk1_rec)
+spk2_rec = torch.stack(spk2_rec)
+
+print(mem2_rec.squeeze(1), spk=spk2_rec.squeeze(1))
