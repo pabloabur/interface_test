80faebbf71f73737141a469b354d323a4a3232a8
pabloabur <pabloabur@gmail.com>
2024-08-14 17:48:53 +1000

    Populates data frames with network information

--- test_brian.py
+++ test_brian.py
@@ -23,27 +23,60 @@ def visualise_connectivity(S):
     plt.xlabel('Source neuron index')
     plt.ylabel('Target neuron index')
 
-def create_lookup_table(S, neurons_per_core, cores_per_pop):
-    num_source_neu = len(S.i)
+def create_lookup_table(num_neurons, neurons_per_core, cores_per_pop):
+    neurons_per_pop = neurons_per_core * cores_per_pop
+    population_slices = int(np.ceil(num_neurons/neurons_per_pop))
+    core_slices = int(neurons_per_pop/neurons_per_core)
 
-    cores = [neurons_per_core for _ in range(int(num_source_neu/neurons_per_core))]
-    pre_cores = np.array_split(S.i, np.cumsum(cores))
-    post_cores = np.array_split(S.j, np.cumsum(cores))
+    population_index = [x for x in range(population_slices) for _ in range(neurons_per_pop)]
+    population_index = np.array(population_index)
 
-    # TODO populate it
-    df = pd.DataFrame({
-        "source_index": S.i,
-        "target_index": S.j,
-        "core_index": ,
-        "population_index": ,
-        "neural_type": ,
+    core_index = [x for x in range(core_slices) for _ in range(neurons_per_core)]
+    core_index = np.tile(core_index, population_slices)
+
+    # Some populations may not be fully occupied, so we fix sizes here
+    neuron_index = [x for x in range(num_neurons)]
+    mismatch_size = abs(num_neurons-len(core_index))
+    if mismatch_size:
+        neuron_index.extend([np.nan for _ in range(mismatch_size)])
+
+    internal_index = [x for _ in range(core_slices) for x in range(neurons_per_core)]
+    internal_index = np.tile(internal_index, population_slices)
+
+    df_neurons = pd.DataFrame({
+        "neuron_index": neuron_index,
+        "internal_index": internal_index,
+        "core_index": core_index,
+        "population_index": population_index,
         })
 
-N = 10
+    source_id = np.array(S.i).tolist()
+    target_id = np.array(S.j).tolist()
+    source_population = df_neurons.loc[source_id]['population_index'].to_numpy()
+    source_core = df_neurons.loc[source_id]['core_index'].to_numpy()
+    source_internal_index = df_neurons.loc[source_id]['internal_index'].to_numpy()
+    target_population = df_neurons.loc[target_id]['population_index'].to_numpy()
+    target_core = df_neurons.loc[target_id]['core_index'].to_numpy()
+    target_internal_index = df_neurons.loc[target_id]['internal_index'].to_numpy()
+
+    import pdb;pdb.set_trace()
+
+    df_synapses = pd.DataFrame({
+        "source_id": source_id,
+        "source_population": source_population,
+        "source_core": source_core,
+        "source_internal_index": source_internal_index,
+        "target_id": target_id,
+        "target_population": target_population,
+        "target_core": target_core,
+        "target_internal_index": target_internal_index,
+        })
+
+num_neurons = 10
 #con_prob = 0.2
 con_prob = 0.5
 #con_prob = 1.0
-G = NeuronGroup(N, 'v:1')
+G = NeuronGroup(num_neurons, 'v:1')
 S = Synapses(G, G)
 S.connect(condition='i!=j', p=con_prob)
 
@@ -52,5 +85,5 @@ neurons_per_core = 2
 cores_per_pop = 2
 
 visualise_connectivity(S)
-create_lookup_table(S, neurons_per_core, cores_per_pop)
+create_lookup_table(num_neurons, neurons_per_core, cores_per_pop)
 plt.show()
