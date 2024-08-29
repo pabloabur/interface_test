35cf59dbcfeb096718274b38096f69f7a47b2a3f
pabloabur <pabloabur@gmail.com>
2024-08-29 18:01:12 +1000

    WIP improves lut generation

--- test_brian.py
+++ test_brian.py
@@ -1,4 +1,4 @@
-from brian2 import NeuronGroup, Synapses
+from brian2 import NeuronGroup, Synapses, Network, collect
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -23,67 +23,112 @@ def visualise_connectivity(S):
     plt.xlabel('Source neuron index')
     plt.ylabel('Target neuron index')
 
-def create_lookup_table(num_neurons, neurons_per_core, cores_per_pop):
-    neurons_per_pop = neurons_per_core * cores_per_pop
-    population_slices = int(np.ceil(num_neurons/neurons_per_pop))
-    core_slices = int(neurons_per_pop/neurons_per_core)
-
-    population_index = [x for x in range(population_slices) for _ in range(neurons_per_pop)]
-    population_index = np.array(population_index)
-
-    core_index = [x for x in range(core_slices) for _ in range(neurons_per_core)]
-    core_index = np.tile(core_index, population_slices)
-
-    # Some populations may not be fully occupied, so we fix sizes here
+def create_lookup_table(neurons_per_core, network, recurrent_index):
+    num_neurons = sum([x.N for x in network.objects if isinstance(x, NeuronGroup)])
+    num_cores = int(np.ceil(num_neurons/neurons_per_core))
+    core_index = [x for x in range(num_cores)]
+    # Some cores may not be fully occupied, so we fix sizes here
     neuron_index = [x for x in range(num_neurons)]
-    mismatch_size = abs(num_neurons-len(core_index))
+    mismatch_size = abs(num_neurons - num_cores*neurons_per_core)
     if mismatch_size:
         neuron_index.extend([np.nan for _ in range(mismatch_size)])
+    intracore_index = [x for _ in range(num_cores) for x in range(neurons_per_core)]
 
-    internal_index = [x for _ in range(core_slices) for x in range(neurons_per_core)]
-    internal_index = np.tile(internal_index, population_slices)
+    # Each subcore in a core memory matrix has neurons_per_subcore. 8 by default
+    num_subcores = 8
+    neurons_per_subcore = int(np.ceil(neurons_per_core / num_subcores))
+    # Considering 3 state varibles, each with 8-bits. 
+    matrix_num_rows = 3 * num_subcores
+    state_variables = np.empty((num_cores*matrix_num_rows, neurons_per_subcore),
+                               dtype=np.int8)
+
+    def map_synapses(syn):
+        # TODO
+        # TODO indices relative to groups and I don't know total number of neurons
+        # 	maps neurons to matrix and manage core/neu map?
+        #	maybe anchor target neuron group and build map interactively
+        # [syn.target==neurons[0] for syn in synapses]?
+        pass
+
+    def set_states(states, target_neurons):
+        """
+        Parameters
+        ----------
+        states : ndarray
+            Matrix with state variables.
+        target_neurons : ndarray
+        -------
+        """
+        pass
+    neurons = [x for x in network.objects if isinstance(x, NeuronGroup)]
+    index_pointer = 0
+    for neu in neurons:
+        states = neu.get_states(['v', 'x', 'I'])
+        for state in states:
+            pass
+
+    import pdb; pdb.set_trace()
+    #np.array_split(neurons[0].get_states(['v', 'x', 'I'])['v'], np.cumsum([neurons_per_core for _ in range(num_blocks)]))
 
     df_neurons = pd.DataFrame({
-        "neuron_index": neuron_index,
-        "internal_index": internal_index,
-        "core_index": core_index,
-        "population_index": population_index,
+        "neuron": intracore_index,
+        "core": core_index,
         })
+
+    # TODO get neuron and synapse groups from net. syn.N from i or j. need to map groups
+    # TODO map neurons to memory
+    # TODO map synapses
+    synapses = [x for x in network.objects if isinstance(x, Synapses)]
+    # TODO two different populations targetting the same population should not create new entries in matrices created here
+    for syn in synapses:
+        if recurrent_index:
+            # TODO recurrent connectivity pattern
+            pass
 
     source_id = np.array(S.i).tolist()
     target_id = np.array(S.j).tolist()
-    source_population = df_neurons.loc[source_id]['population_index'].to_numpy()
     source_core = df_neurons.loc[source_id]['core_index'].to_numpy()
-    source_internal_index = df_neurons.loc[source_id]['internal_index'].to_numpy()
-    target_population = df_neurons.loc[target_id]['population_index'].to_numpy()
+    source_internal_index = df_neurons.loc[source_id]['intracore_index'].to_numpy()
     target_core = df_neurons.loc[target_id]['core_index'].to_numpy()
-    target_internal_index = df_neurons.loc[target_id]['internal_index'].to_numpy()
-
-    import pdb;pdb.set_trace()
+    target_internal_index = df_neurons.loc[target_id]['intracore_index'].to_numpy()
 
     df_synapses = pd.DataFrame({
         "source_id": source_id,
-        "source_population": source_population,
         "source_core": source_core,
         "source_internal_index": source_internal_index,
         "target_id": target_id,
-        "target_population": target_population,
         "target_core": target_core,
         "target_internal_index": target_internal_index,
         })
+
+    return df_neurons, df_synapses
 
 num_neurons = 10
 #con_prob = 0.2
 con_prob = 0.5
 #con_prob = 1.0
-G = NeuronGroup(num_neurons, 'v:1')
-S = Synapses(G, G)
-S.connect(condition='i!=j', p=con_prob)
+G1 = NeuronGroup(num_neurons,
+                 '''v:1
+                    I:1
+                    x:1''',
+                 name="population_1")
+G2 = NeuronGroup(num_neurons,
+                 '''v:1
+                    I:1
+                    x:1''',
+                 name="population_2")
+S = Synapses(G1, G2)
+S.connect(p=con_prob)
+Sr = Synapses(G1, G1)
+Sr.connect(p=con_prob)
+
+# TODO another model:1k -> 1k, with 1-2 (if recurrent), 1-1024, 1-1 (to other pop), 1024-1 (to other pop)
+
+net = Network(collect())
 
 # Hardware mapping
-neurons_per_core = 2
-cores_per_pop = 2
+neurons_per_core = 16
 
 visualise_connectivity(S)
-create_lookup_table(num_neurons, neurons_per_core, cores_per_pop)
+df_neurons, df_synapses = create_lookup_table(neurons_per_core, net, [1])
 plt.show()
