f64c5bbaca99871975805b662d188bcc0b1a787e
pabloabur <pabloabur@gmail.com>
2024-08-30 18:46:03 +1000

    WIP creates temporary conventional memory tables

--- test_brian.py
+++ test_brian.py
@@ -23,6 +23,37 @@ def visualise_connectivity(S):
     plt.xlabel('Source neuron index')
     plt.ylabel('Target neuron index')
 
+# TODO
+def preprocess_network(network):
+    #   indices relative to groups and I don't know total number of neurons
+    #	maybe anchor target neuron group and build map interactively
+    #   [syn.target==neurons[0] for syn in synapses]?
+    #   map index to big array where starting point is Nc*Cs + Nnc*ns
+    neurons = [x for x in network.objects if isinstance(x, NeuronGroup)]
+    num_neurons = sum([x.N for x in neurons])
+    synapses = [x for x in network.objects if isinstance(x, Synapses)]
+
+    current_table = np.zeros(num_neurons)
+    voltage_table = np.zeros(num_neurons)
+    trace_table = np.zeros(num_neurons)
+
+    global_neuron_id = np.array([])
+    for neu in neurons:
+        temp_length = len(global_neuron_id)
+        temp_slice = temp_length + neu.i
+        global_neuron_id = np.concatenate((global_neuron_id, temp_slice))
+        current_table[temp_slice] = neu.get_states(['I'])['I']
+        voltage_table[temp_slice] = neu.get_states(['v'])['v']
+        trace_table[temp_slice] = neu.get_states(['x'])['x']
+
+    # Each row contains the initial an address for a row in a weight table
+    #index_pointer = 0
+    #for neu in neurons:
+    #    states = neu.get_states(['v', 'x', 'I'])
+    #    for state in states:
+    #        pass
+    #np.array_split(neurons[0].get_states(['v', 'x', 'I'])['v'], np.cumsum([neurons_per_core for _ in range(num_blocks)]))
+
 def create_lookup_table(neurons_per_core, network, recurrent_index):
     num_neurons = sum([x.N for x in network.objects if isinstance(x, NeuronGroup)])
     num_cores = int(np.ceil(num_neurons/neurons_per_core))
@@ -42,33 +73,15 @@ def create_lookup_table(neurons_per_core, network, recurrent_index):
     state_variables = np.empty((num_cores*matrix_num_rows, neurons_per_subcore),
                                dtype=np.int8)
 
-    def map_synapses(syn):
-        # TODO
-        # TODO indices relative to groups and I don't know total number of neurons
-        # 	maps neurons to matrix and manage core/neu map?
-        #	maybe anchor target neuron group and build map interactively
-        # [syn.target==neurons[0] for syn in synapses]?
-        pass
-
-    def set_states(states, target_neurons):
-        """
-        Parameters
-        ----------
-        states : ndarray
-            Matrix with state variables.
-        target_neurons : ndarray
-        -------
-        """
-        pass
-    neurons = [x for x in network.objects if isinstance(x, NeuronGroup)]
-    index_pointer = 0
-    for neu in neurons:
-        states = neu.get_states(['v', 'x', 'I'])
-        for state in states:
-            pass
-
-    import pdb; pdb.set_trace()
-    #np.array_split(neurons[0].get_states(['v', 'x', 'I'])['v'], np.cumsum([neurons_per_core for _ in range(num_blocks)]))
+    # TODO improve and place this somewhere else
+    fig, axes = plt.subplots(2, 1)
+    axes[0].imshow(state_variables[0:matrix_num_rows, :], aspect='auto')
+    axes[1].imshow(state_variables[matrix_num_rows:2*matrix_num_rows, :], aspect='auto')
+    for i in range(2):
+        axes[0].add_patch(plt.Rectangle((0, 2*i), 1, 1, color='r'))
+        axes[1].add_patch(plt.Rectangle((0, 2*i), 1, 1, color='r'))
+    axes[0].text(0, 0, 'v', color='r')
+    plt.show()
 
     df_neurons = pd.DataFrame({
         "neuron": intracore_index,
@@ -76,9 +89,6 @@ def create_lookup_table(neurons_per_core, network, recurrent_index):
         })
 
     # TODO get neuron and synapse groups from net. syn.N from i or j. need to map groups
-    # TODO map neurons to memory
-    # TODO map synapses
-    synapses = [x for x in network.objects if isinstance(x, Synapses)]
     # TODO two different populations targetting the same population should not create new entries in matrices created here
     for syn in synapses:
         if recurrent_index:
